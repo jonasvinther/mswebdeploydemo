a8fc5b7cd30e54ed77740c8f635f24794735ad4d
Jonas Vinther <jrvinther@gmail.com>
2017-09-28 15:02:48 +0200

    Clean up

--- build_scripts/build.ps1
+++ build_scripts/build.ps1
@@ -5,4 +5,4 @@ param(
 
 &"C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/amd64/MSBuild.exe" `
 ${project_path}/WebApplication1.csproj `
-/v:detailed /t:ReBuild;Package /p:Configuration=Release
+/v:"detailed" /t:"ReBuild,Package" /p:"Configuration=Release"
